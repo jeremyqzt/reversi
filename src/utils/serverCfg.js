ef7aab6322264b90219d55880a158db38df947e4
jeremyqzt <jq357@hotmail.com>
2020-07-10 17:39:25 -0400

    server comm using fetch instead of axios

---
+++ src/utils/serverCfg.js
@@ -0,0 +1,6 @@
+const serverCfg = {
+    server: "http://127.0.0.1:8000",
+    cors: "cors",
+}
+
+export default serverCfg;
---
+++ src/utils/serverComm.js
@@ -0,0 +1,43 @@
+import JwtUtils from './jwtUtils.js';
+import serverCfg from './serverCfg.js';
+
+class serverComm{
+    static async post(data = {}, url) {
+        // Default options are marked with *
+        const response = fetch(`${serverCfg.server}/${url}`, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: serverCfg.cors, // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `JWT ${JwtUtils.getAccessToken()}`,
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response;
+      }
+
+
+      static async get(data = {}, url) {
+        // Default options are marked with *
+        const response = fetch(`${serverCfg.server}/${url}`, {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: serverCfg.cors, // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `JWT ${JwtUtils.getAccessToken()}`,
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response;
+      }
+}
+
+export default serverComm;
