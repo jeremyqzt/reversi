70daf8db4679c11ce79b8d2a80e199f006e030cd
jeremyqzt <jq357@hotmail.com>
2020-07-19 11:40:44 -0400

    django can serve webpages now

--- server/reversiServer/reversiServer/urls.py
+++ server/reversiServer/reversiServer/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.contrib.staticfiles.views import serve
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('api/', include('game.urls')),
     path('auth/', include('login.urls')),
     path('lobby/', include('lobby.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root="/")
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,21 +4,21 @@ class Rules extends Component {
     render(){
       return (
           <div>
-            <h3 className="mt-4"><span role="img" aria-label="basic">📖</span> Basic Rules</h3>
-            <ul className="list-group list-group-flush">
+            <h3 className="mt-4 fade-in to-be-animated" style={{animationDelay: "0.25s"}}><span role="img" aria-label="basic">📖</span> Basic Rules</h3>
+            <ul className="list-group list-group-flush fade-in to-be-animated" style={{animationDelay: "0.25s"}}>
                 <li className="list-group-item">The game is played on a 8x8 board</li>
                 <li className="list-group-item">Alternating initial pieces are allocated in the 4 center spaces</li>
                 <li className="list-group-item">Place flanking pieces to capture all vertically, horizontally and diagnally flanked pieces</li>
                 <li className="list-group-item">A move must be placed on an empty square and MUST capture atleast 1 pieces</li>
                 <li className="list-group-item">When there are no valid moves remaining, the color with the most pieces wins</li>
             </ul>
-            <h3 className="mt-4"><span role="img" aria-label="special">🔖</span> Special Rules</h3>
-            <ul className="list-group list-group-flush">
+            <h3 className="mt-4 fade-in to-be-animated" style={{animationDelay: "0.5s"}}><span role="img" aria-label="special">🔖</span> Special Rules</h3>
+            <ul className="list-group list-group-flush fade-in to-be-animated" style={{animationDelay: "0.5s"}}>
                 <li className="list-group-item">Black pieces always goes first</li>
                 <li className="list-group-item">If a played has no valid moves, the other player will gain an additional turn</li>
             </ul>
-            <h3 className="mt-4"><span role="img" aria-label="ai">🤖</span> AI Notes</h3>
-            <ul className="list-group list-group-flush">
+            <h3 className="mt-4 fade-in to-be-animated" style={{animationDelay: "0.75s"}}><span role="img" aria-label="ai">🤖</span> AI Notes</h3>
+            <ul className="list-group list-group-flush fade-in to-be-animated" style={{animationDelay: "0.75s"}}>
                 <li className="list-group-item">When playing the AI, the player always gets to go first</li>
                 <li className="list-group-item">The <i><span role="img" aria-label="ez">🍂</span> EZ</i> AI will make a random move</li>
                 <li className="list-group-item">The <i><span role="img" aria-label="ok">🌱</span> Okay AI</i> will make the best current move</li>
