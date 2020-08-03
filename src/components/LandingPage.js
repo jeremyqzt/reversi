fb58c4055019ece155a529534e26b5d652ee9a39
jeremyqzt <jq357@hotmail.com>
2020-08-03 09:32:53 -0400

    changed text on landing page...

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,8 +8,8 @@ class LandingPage extends Component {
         <div className="reversiColors">
             <div className="d-flex fullScreen">
                 <div className="justify-content-center center text-center">
-                    <h1 className="noSelect reversiHeader big">Online Reversi</h1>
-                    <a href="/login" role="button" className="btn btn-primary" style={{color: "lavender", backgroundColor: "#505160", borderColor: "lavender"}}>Play Now!</a>
+                    <h1 className="noSelect reversiHeader big">Reversi</h1>
+                    <a href="/login" role="button" className="btn btn-primary" style={{color: "lavender", backgroundColor: "#505160", borderColor: "lavender"}}>Get Started</a>
                 </div>
             </div>
         </div>
