69bef565ade3fa9d0a20ea82c06bf7d255f16205
jeremyqzt <jq357@hotmail.com>
2020-08-04 14:18:56 -0400

    daemonize uwsgi correctly

--- install.sh
+++ install.sh
@@ -16,12 +16,12 @@ ln -sf /etc/nginx/sites-available/reversi.conf /etc/nginx/sites-enabled/
 
 cd server/reversiServer/
 
-/home/jeremyq357/.local/bin/uwsgi --daemonize \
+/home/jeremyq357/.local/bin/uwsgi \
             -s /home/jeremyq357/reversi/server/sockets/sock1 --module reversiServer.wsgi \
-            --disable-logging --static-map /=/home/jeremyq357/reversi/server/reversiServer/build --chmod-socket=666 
+            --disable-logging --static-map /=/home/jeremyq357/reversi/server/reversiServer/build --chmod-socket=666 --daemonize /var/log/reversi1.log
 
 /home/jeremyq357/.local/bin/uwsgi --daemonize \
             -s /home/jeremyq357/reversi/server/sockets/sock2 --module reversiServer.wsgi \
-            --disable-logging --static-map /=/home/jeremyq357/reversi/server/reversiServer/build --chmod-socket=666 
+            --disable-logging --static-map /=/home/jeremyq357/reversi/server/reversiServer/build --chmod-socket=666 --daemonize /var/log/reversi2.log
 
 systemctl reload nginx
