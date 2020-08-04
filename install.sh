14a38dafd9e9f68537f3fde914fa71b83154d22f
jeremyqzt <jq357@hotmail.com>
2020-08-04 11:28:19 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ systemctl enable nodeDb
 systemctl enable reversi
 systemctl start nodeDb
 
+mkdir /etc/reversi
+cp systemd/uwsgi_params /etc/reversi/
+
 cp systemd/nginx.conf /etc/nginx/sites-available/reversi.conf
 ln -sf /etc/nginx/sites-available/reversi.conf /etc/nginx/sites-enabled/
 systemctl reload nginx
-
-mkdir /etc/reversi
-cp systemd/uwsgi_params /etc/reversi/
